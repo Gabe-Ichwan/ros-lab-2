4a554ff8164a5d57d41cc226047efdfa4937d384
Gabriel <ichwang@wwu.edu>
2025-04-17 08:28:53 -0700

    removed redundant print

--- wall_follower_pkg/wall_follower_pkg/wall_follower.py
+++ wall_follower_pkg/wall_follower_pkg/wall_follower.py
@@ -11,23 +11,21 @@ class WallFollower(Node):
         self.scan_sub = self.create_subscription(LaserScan, '/gabe_drive/scan', self.scan_callback, 10)
 
     def scan_callback(self, msg):
-        # Get the front distance from the single laser sensor (center of the scan)
-        front_distance = msg.ranges[0]  # Only has a single sensor
+        # Get distances from front and side sensors
+        front_distance = msg.ranges[0]
         side_distance = msg.ranges[1] 
 
         print ("Front distance:", front_distance)
         print ("Side distance:", side_distance)
 
-        self.get_logger().info(f"Front Distance: {front_distance} m")
-
         twist = Twist()
 
-        if (front_distance < 3.0):  # Too close, turn
-            twist.angular.z = -2.0  # Turn to avoid collision
+        if (front_distance < 3.0):  # Turn if forward wall is close
+            twist.angular.z = -2.0
         elif (front_distance > 3.0) and (side_distance < 2.5):
             twist.linear.x = 10.0  # Move forward
         elif (side_distance > 2.5):
-            twist.linear.x = 1.0  # Move forward and turn if left wall distant
+            twist.linear.x = 1.0  # Move forward and turn left if left wall distant
             twist.angular.z = 0.3
 
         self.cmd_vel_pub.publish(twist)
